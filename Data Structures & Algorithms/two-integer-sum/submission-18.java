76211987938a7497fc8fe37d5c219daa71ea5b3f
TED <teddy5518@hotmail.com>
2026-05-13 23:38:25 -0400

    Add: two-integer-sum - submission-18

---
+++ Data Structures & Algorithms/two-integer-sum/submission-18.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        // HashMap
+        // Time Complexity: O(n)
+        // Space Complexity: O(n)
+
+        HashMap<Integer, Integer> seen = new HashMap<Integer, Integer>();
+
+        for (int i = 0; i < nums.length; i++) {
+            seen.put(nums[i], i);
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            int diff = target - nums[i];
+
+            if (seen.containsKey(diff) && seen.get(diff) != i) return new int[] { i , seen.get(diff) };
+        }
+
+        return new int[0];
+    }
+}
