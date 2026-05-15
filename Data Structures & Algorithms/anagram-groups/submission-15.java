9941c7bd1bd50f109ec6bfb6c509edffa6b9ef61
TED <teddy5518@hotmail.com>
2026-05-14 23:55:16 -0400

    Add: anagram-groups - submission-15

---
+++ Data Structures & Algorithms/anagram-groups/submission-15.java
@@ -0,0 +1,22 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        // HashMap 
+        // Time complexity: O(m*n)
+        // Space complexity: O(m*n) output groups are counted
+        HashMap<String, List<String>> ans = new HashMap<>();
+
+        for (String s : strs) {
+            int[] count = new int[26];
+
+            for (char c : s.toCharArray()) {
+                count[c - 'a']++;
+            }
+
+            String key = Arrays.toString(count);
+            ans.putIfAbsent(key, new ArrayList<>());
+            ans.get(key).add(s);
+        }
+
+        return new ArrayList<>(ans.values());
+    }
+}
