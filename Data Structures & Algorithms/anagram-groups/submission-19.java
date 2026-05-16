83b3974027fd6d3d0b077489a5494c23141383a2
TED <teddy5518@hotmail.com>
2026-05-15 22:52:57 -0400

    Add: anagram-groups - submission-19

---
+++ Data Structures & Algorithms/anagram-groups/submission-19.java
@@ -0,0 +1,23 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        // HashMap
+        // Time complexity: O(n * m)
+        // Space complexity: O(n * m)
+        HashMap<String, List<String>> ans = new HashMap<>();
+
+        for (String s : strs) {
+            int[] count = new int[26]; // a - z
+
+            for (char c : s.toCharArray()) {
+                count[c - 'a']++;
+            }
+
+            String key = Arrays.toString(count);
+
+            ans.putIfAbsent(key, new ArrayList<>());
+            ans.get(key).add(s);
+        }
+
+        return new ArrayList<>(ans.values());
+    }
+}
