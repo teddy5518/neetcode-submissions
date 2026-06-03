d32d57130ab4b8f864831171846cb897ea1b4f93
TED <teddy5518@hotmail.com>
2026-06-03 00:49:09 -0400

    Add: buy-and-sell-crypto - submission-2

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-2.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        // Two Pointers
+        int max = 0;
+
+        for (int i = 0; i < prices.length; i++) {
+            for (int j = i + 1; j < prices.length; j++) {
+                int profit = prices[j] - prices[i];
+                if (profit > max) {
+                    max = profit;
+                }
+            }
+        }
+
+        return max;
+    }
+}
